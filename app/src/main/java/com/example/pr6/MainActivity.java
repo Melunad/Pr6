84ce42a9a002e5120cad7943edb076515b375a5e
Daniil <bans1111111111@gmail.com>
2023-04-22 14:06:58 +0300

    Пункт 2

--- app/src/main/java/com/example/pr6/MainActivity.java
+++ app/src/main/java/com/example/pr6/MainActivity.java
@@ -1,62 +1,36 @@
 package com.example.pr6;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.renderscript.RenderScript;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    private NotificationManager notificationManager;
-    public  static final int NOTIFY_ID = 1;
-    public  static final String CHANEL_ID = "CHANEL_ID";
-    Button button;
+    private EditText editTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-
-        notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                PendingIntent  pendingIntent = PendingIntent.getActivity(getApplicationContext(),0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                NotificationCompat.Builder notificationBuilder =
-                        new NotificationCompat.Builder(getApplicationContext(), CHANEL_ID)
-                                .setAutoCancel(false)
-                                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                                .setWhen(System.currentTimeMillis())
-                                .setContentIntent(pendingIntent)
-                                .setContentTitle("Заголовок")
-                                .setContentText("Текст уведомления");
-                createChannelIfNeeded(notificationManager);
-                notificationManager.notify(NOTIFY_ID, notificationBuilder.build());
-
-            }
-        });
-    }
-    public static void createChannelIfNeeded(NotificationManager manager){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel notificationChannel = new NotificationChannel(CHANEL_ID, CHANEL_ID, NotificationManager.IMPORTANCE_DEFAULT);
-            manager.createNotificationChannel(notificationChannel);
-        }
+        editTextInput = findViewById(R.id.edit_text_input);
     }
 
+    public void startService(View v) {
+        String input = editTextInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
+    }
 }
